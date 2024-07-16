b2815542bb043018784a463ea5e87f364f3ef2e1
Morrison <aka830709@gmail.com>
2024-07-16 11:49:13 +0800

    test

--- SemiTrailer/Program.cs
+++ SemiTrailer/Program.cs
@@ -12,11 +12,8 @@ Startup.ConfigureServices(builder);
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseMiddleware<HttpTenantResolveMiddleware>();
 app.UseHttpsRedirection();
 
